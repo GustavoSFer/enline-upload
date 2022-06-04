04cc0e4f9b2234234cd0a9b4dd9082d9e96deca9
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-06-04 13:48:13 -0300

    Test: Criando teste get quando tem informaçoes

---
+++ back-end/src/tests/Mock/mockFile.js
@@ -0,0 +1,18 @@
+const filesDb = [
+  {
+    "_id": "629b73e4a33062a2c67e80dc",
+    "file": "uploads/Captura de tela de 2022-05-19 17-35-39.png",
+    "name": "Captura de tela de 2022-05-19 17-35-39.png",
+    "__v": 0
+  },
+  {
+    "_id": "629b73eca33062a2c67e8366",
+    "file": "uploads/Captura de tela de 2022-05-19 15-45-25.png",
+    "name": "Captura de tela de 2022-05-19 15-45-25.png",
+    "__v": 0
+  }
+]
+
+module.exports = {
+  filesDb,
+}
---
+++ back-end/src/tests/model/model.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const Sinon = require('sinon');
+const model = require('../../ConnectionMongo');
+const fileModel = require('../../model');
+const { filesDb } = require('../Mock/mockFile');
+
+describe('Model', () => {
+  describe('#fetchFiles', () => {
+    afterEach(Sinon.restore);
+    it('Quando existe dados no DB deve retornar um array com as informações', async () => {
+      Sinon.stub(model, 'find').resolves(filesDb);
+      const files = await fileModel.fetchFiles();
+      expect(files).to.deep.eq(filesDb);
+    });
+  });
+});
